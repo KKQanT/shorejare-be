b7868e775561383302e6be9c5d1059bb2c7f2416
Peerakarn Jitpukdee <thanadule.k@siametrics.com>
2025-06-03 10:28:09 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,7 +19,18 @@ async function bootstrap() {
     exposedHeaders: ['Content-Disposition'],
   });
   
+  // Setup Swagger
+  const config = new DocumentBuilder()
+    .setTitle('ShareJore API')
+    .setDescription('The ShareJore API documentation')
+    .setVersion('1.0')
+    .addTag('sharejore')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   await app.listen(3001);
   console.log('ShareJore AI backend is running on port 3001');
+  console.log('API Documentation available at http://localhost:3001/api');
 }
 bootstrap();
