6fadc65c71bcbe89cd76a21b01e4e8d70229e2bd
Peerakarn Jitpukdee <thanadule.k@siametrics.com>
2025-06-03 10:35:11 +0700

    add image dto

--- src/image/dto/image-analyze.dto.ts
+++ src/image/dto/image-analyze.dto.ts
@@ -1,6 +1,12 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ImageAnalyzeDto {
+  @ApiProperty({
+    description: 'The filename of the uploaded image to analyze',
+    example: '1632748505123-123456789.jpg',
+    required: true
+  })
   @IsString()
   @IsNotEmpty({ message: 'Filename is required' })
   filename: string;
